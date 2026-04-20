37c7cfde5af53841f6f0ceb46e96aa37a1205fa2
Melissa Marcielo <melissamarcielo@MacBook-Air-de-Wilson.local>
2026-04-20 19:36:34 -0300

    atualizacao do authmiddleware pra jwt

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -26,7 +26,7 @@ function authMiddleware(req, res, next) {
       });
     }
 
-    const decoded = jwt.verify(token, "segredo_super_secreto");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.usuario = {
       id: decoded.id,
